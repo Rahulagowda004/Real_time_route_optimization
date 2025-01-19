bd9263a03fca91c445f5cc6d899204b3401cd158
rahulagowda277 <rahulgowda277@gmail.com>
2025-01-19 13:18:17 +0530

    updated so many things in prediction.py

--- src/pipeline/prediction.py
+++ src/pipeline/prediction.py
@@ -8,45 +8,51 @@ from src.utils.utils import load_object
 
 class preprocess:
     def __init__(self):
-        pass   
-    
-    # Load pre-trained preprocessor objects
-    avg_delivery_time_area = joblib.load('avg_delivery_time_area.pkl')
-    traffic_weather_impact = joblib.load('traffic_weather_impact.pkl')
-    max_deliveries_per_vehicle = joblib.load('max_deliveries_per_vehicle.pkl')
+        # Load pre-trained preprocessor objects
+        self.avg_delivery_time_area = joblib.load('artifacts/preprocessor/avg_delivery_time_area.pkl')
+        self.traffic_weather_impact = joblib.load('artifacts/preprocessor/traffic_weather_impact.pkl')
+        self.max_deliveries_per_vehicle = joblib.load('artifacts/preprocessor/max_deliveries_per_vehicle.pkl')
+
     
     def clean_df(self,df):
-        def add_features_for_prediction(self,df):
-            df['avg_delivery_time_area'] = df['City'].map(self.avg_delivery_time_area)
-            
-            df['traffic_weather_impact'] = df.apply(
-                lambda row: self.traffic_weather_impact.get((row['Road_traffic_density'], row['Weatherconditions']), None),
-                axis=1
-            )
-            
-            df['vehicle_capacity_utilization'] = df.apply(
-                lambda row: row['multiple_deliveries'] / self.max_deliveries_per_vehicle.get(row['Type_of_vehicle'], 1),
-                axis=1
-            ).fillna(0)
-            
-            def calculate_distance(row):
-                restaurant_coords = (row['translogi_latitude'], row['translogi_longitude'])
-                delivery_coords = (row['Delivery_location_latitude'], row['Delivery_location_longitude'])
-                return geodesic(restaurant_coords, delivery_coords).kilometers
-            df['distance'] = df.apply(calculate_distance, axis=1)
-            return df
         
-        df.drop(['ID'], axis=1, inplace=True) #dropping the ID column(irrelavant)
-        df['Order_Date']=pd.to_datetime(df['Order_Date'])
-        df['Order_day']=df['Order_Date'].dt.day
-        df['Order_month']=df['Order_Date'].dt.month
-        df['Order_year']=df['Order_Date'].dt.year
-        df['Time_Orderd'] = pd.to_datetime(df['Time_Orderd'])
-        df['Hour_order']=df['Time_Orderd'].dt.hour
-        df['Min_order']=df['Time_Orderd'].dt.minute
-        df.drop(["Time_Orderd", "Order_Date"],axis = 1, inplace= True)  
-        df = add_features_for_prediction(df)
-        return df
+        try:
+            def add_features_for_prediction(df):
+                try:
+                    df['avg_delivery_time_area'] = df['City'].map(self.avg_delivery_time_area)
+                    
+                    df['traffic_weather_impact'] = df.apply(
+                        lambda row: self.traffic_weather_impact.get((row['Road_traffic_density'], row['Weatherconditions']), None),
+                        axis=1
+                    )
+                    
+                    df['vehicle_capacity_utilization'] = df.apply(
+                        lambda row: row['multiple_deliveries'] / self.max_deliveries_per_vehicle.get(row['Type_of_vehicle'], 1),
+                        axis=1
+                    ).fillna(0)
+                    
+                    def calculate_distance(row):
+                        restaurant_coords = (row['translogi_latitude'], row['translogi_longitude'])
+                        delivery_coords = (row['Delivery_location_latitude'], row['Delivery_location_longitude'])
+                        return geodesic(restaurant_coords, delivery_coords).kilometers
+                    df['distance'] = df.apply(calculate_distance, axis=1)
+                    return df
+                except Exception as e:
+                    raise CustomException(e,sys)
+            
+            df.drop(['ID'], axis=1, inplace=True) #dropping the ID column(irrelavant)
+            df['Order_Date']=pd.to_datetime(df['Order_Date'])
+            df['Order_day']=df['Order_Date'].dt.day
+            df['Order_month']=df['Order_Date'].dt.month
+            df['Order_year']=df['Order_Date'].dt.year
+            df['Time_Orderd'] = pd.to_datetime(df['Time_Orderd'])
+            df['Hour_order']=df['Time_Orderd'].dt.hour
+            df['Min_order']=df['Time_Orderd'].dt.minute
+            df.drop(["Time_Orderd", "Order_Date"],axis = 1, inplace= True)  
+            df = add_features_for_prediction(df)
+            return df
+        except Exception as e:
+            raise CustomException(e,sys)
 
 class PredictPipeline:
     def __init__(self):
@@ -54,7 +60,7 @@ class PredictPipeline:
     
     def predict(self,dataframe):
         try:
-            model_path=os.path.join("artifacts/model","model.pkl")
+            model_path=os.path.join("artifacts/model","best_model.pkl")
             preprocessor_path=os.path.join('artifacts/preprocessor','preprocessor.pkl')
             print("Before Loading")
             model=load_object(file_path=model_path)
@@ -67,6 +73,8 @@ class PredictPipeline:
             data_scaled=preprocessor.transform(dataframe)
             preds=model.predict(data_scaled)
             print("Predictions: ",preds)
+            preds = pd.DataFrame(preds, columns=['Predictions'])
+            preds.to_csv("artifacts/Prediction/Predictions.csv", index=False)
             return preds
         
         except Exception as e:
@@ -141,12 +149,12 @@ class CustomData:
         except Exception as e:
             raise CustomException(e, sys)
         
-    df = get_data_as_data_frame()
-    obj = PredictPipeline()
-    time_taken = obj.predict(df)
-    print("The time consuming for the delivery would be: ", time_taken)
+    # df = self.get_data_as_data_frame()
+    # obj = PredictPipeline()
+    # time_taken = obj.predict(df)
+    # print("The time consuming for the delivery would be: ", time_taken)
         
 if __name__ == "__main__":
     obj=PredictPipeline()
-    df = pd.read_csv("data/test.csv")
+    df = pd.read_csv("artifacts/Prediction/test.csv")
     obj.predict(df)
