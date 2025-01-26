4d7417ec91699653d96802e7e4b9ba67c4b69ad3
rahulagowda277 <rahulgowda277@gmail.com>
2025-01-26 08:17:35 +0530

    preprocessor is working

---
+++ src/components/custom_transformer.py
@@ -0,0 +1,13 @@
+# custom_transformer.py
+from sklearn.pipeline import TransformerMixin
+
+class FilterOutBigValuesTransformer(TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y=None):
+        self.biggest_value = X.c1.max()
+        return self
+
+    def transform(self, X):
+        return X.loc[X.c1 <= self.biggest_value]
